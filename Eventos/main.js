d673fbe7e0fe8d01e389cd02f47a07e2c77aec83
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-09-25 19:19:20 -0700

    Eventos y CRUD

---
+++ Eventos/main.js
@@ -0,0 +1,12 @@
+console.log("Probando...");
+
+const saludar = () => {
+  alert("Saludar");
+};
+
+console.log(document);
+
+const leerInput = () => {
+  const valorInput = document.getElementById("nombre").value;
+  console.log(valorInput);
+};
